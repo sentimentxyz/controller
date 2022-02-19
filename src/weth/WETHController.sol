605686af8a30f71676510fcb39904126881518f5
ruvaag <ruvaag@gmail.com>
2022-02-20 00:58:33 +0530

    refactor: optimize WETHController

--- src/weth/WETHController.sol
+++ src/weth/WETHController.sol
@@ -6,15 +6,8 @@ import {IController} from "../core/IController.sol";
 contract WETHController is IController {
     bytes4 constant DEPOSIT = 0xd0e30db0;
     bytes4 constant WITHDRAW = 0x2e1a7d4d;
-    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address[] weth = new address[](1);
-
-    constructor() {
-        // the outputs are already known and the sig only affects their
-        // order so initialize the arrays beforehand and return as per sig
-        weth[0] = WETH;
-    }
-
+    address[] weth = [ 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 ]; // WETH9
+    
     function canCall(
         address,
         bytes calldata data

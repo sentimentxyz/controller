e528f8c340b2faf14908e258c52a040782d8bc4b
r0ohafza <coldroohafza@gmail.com>
2022-03-10 15:52:46 -0800

    feat: allow swapping using Eth

--- src/curve/CurveController.sol
+++ src/curve/CurveController.sol
@@ -7,27 +7,39 @@ import {IControllerFacade} from "../core/IControllerFacade.sol";
 
 contract CurveController is IController {
     IControllerFacade public immutable controllerFacade;
-    bytes4 public constant EXCHANGE = 0x5b41b908;
+    bytes4 public constant EXCHANGE = 0x394747c5;
 
     constructor(IControllerFacade _controllerFacade) {
         controllerFacade = _controllerFacade;
     }
 
-    function canCall(
-        address target,
-        bytes calldata data
-    ) external view returns (bool, address[] memory, address[] memory)  
-    {        
-        if (bytes4(data) != EXCHANGE) 
+    function canCall(address target, bytes calldata data) 
+        external
+        view
+        returns (bool, address[] memory, address[] memory)  
+    {   
+        // validate signature
+        if (bytes4(data) != EXCHANGE)
             return (false, new address[](0), new address[](0));
 
-        (uint256 i, uint256 j) = abi.decode(data[4:], (uint256, uint256));
+        // decode data
+        (uint256 i, uint256 j,,,bool useEth) = abi.decode(
+            data[4:],
+            (uint256, uint256, uint256, uint256, bool)
+        );
 
-        // Prepare Output
         address[] memory tokensIn = new address[](1);
+        tokensIn[0] = IStableSwapPool(target).coins(j);     
+        
+        if (useEth) 
+            return (
+                controllerFacade.isSwapAllowed(tokensIn[0]),
+                tokensIn,
+                new address[](0)
+            );
+
         address[] memory tokensOut = new address[](1);
         tokensOut[0] = IStableSwapPool(target).coins(i);
-        tokensIn[0] = IStableSwapPool(target).coins(j);
         
         return (
             controllerFacade.isSwapAllowed(tokensIn[0]), 
--- src/curve/IStableSwapPool.sol
+++ src/curve/IStableSwapPool.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.8.10;
 
 interface IStableSwapPool {
     function coins(uint256 i) external view returns (address);
-    function get_dy(uint256, uint256, uint256) external view returns (uint256);
 }

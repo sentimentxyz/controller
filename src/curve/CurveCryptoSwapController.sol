8f3965e9902601ae5f33e84adc439120139d7253
r0ohafza <coldroohafza@gmail.com>
2022-04-03 21:29:48 -0700

    feat: add and remove liquidity from curve

--- src/curve/CurveCryptoSwapController.sol
+++ src/curve/CurveCryptoSwapController.sol
@@ -8,6 +8,9 @@ import {IControllerFacade} from "../core/IControllerFacade.sol";
 contract CurveCryptoSwapController is IController {
     IControllerFacade public immutable controllerFacade;
     bytes4 public constant EXCHANGE = 0x394747c5;
+    bytes4 public constant ADD_LIQUIDITY = 0x4515cef3;
+    bytes4 public constant REMOVE_LIQUIDITY = 0xecb586a5;
+    bytes4 public constant REMOVE_LIQUIDITY_ONE_COIN = 0xf1dc3cc9;
 
     constructor(IControllerFacade _controllerFacade) {
         controllerFacade = _controllerFacade;
@@ -18,33 +21,87 @@ contract CurveCryptoSwapController is IController {
         view
         returns (bool, address[] memory, address[] memory)  
     {   
-        // validate signature
-        if (bytes4(data) != EXCHANGE)
-            return (false, new address[](0), new address[](0));
+        bytes4 sig = bytes4(data);
 
-        // decode data
-        (uint256 i, uint256 j) = abi.decode(
-            data[4:],
-            (uint256, uint256)
-        );
+        if (sig == ADD_LIQUIDITY) {
+            (uint256[3] memory amounts,) = abi.decode(
+                data[4:],
+                (uint256[3], uint256)
+            );
+            
+            address[] memory tokensIn = new address[](1);
+            address[] memory tokensOut = new address[](3);
+            
+            tokensIn[0] = IStableSwapPool(target).token();
+            for (uint i=0; i<3; i++) {
+                if (amounts[i] > 0) 
+                    tokensOut[i] = IStableSwapPool(target).coins(i);
+            }
+            return (true, tokensIn, tokensOut);
+        }
 
-        address[] memory tokensIn = new address[](1);
-        tokensIn[0] = IStableSwapPool(target).coins(j);     
-        
-        if (useEth) 
-            return (
-                controllerFacade.isSwapAllowed(tokensIn[0]),
-                tokensIn,
-                new address[](0)
+        if (sig == REMOVE_LIQUIDITY_ONE_COIN) {
+            (,uint256 i, uint256 min_amount) = abi.decode(
+                data[4:],
+                (uint256, uint256, uint256)
+            );
+            
+            if (min_amount == 0)
+                return (false, new address[](0), new address[](0));
+
+            address[] memory tokensIn = new address[](1);
+            address[] memory tokensOut = new address[](1);
+            
+            tokensIn[0] = IStableSwapPool(target).coins(i);
+            tokensOut[0] = IStableSwapPool(target).token();
+
+            return (true, tokensIn, tokensOut);
+        }
+
+        if (sig == REMOVE_LIQUIDITY) {
+            
+            (,uint256[3] memory amounts) = abi.decode(
+                data[4:],
+                (uint256, uint256[3])
+            );
+            
+            address[] memory tokensIn = new address[](3);
+            address[] memory tokensOut = new address[](1);
+            
+            for (uint i=0; i < 3; i++)
+                if (amounts[i] > 0) 
+                    tokensIn[i] = IStableSwapPool(target).coins(i);
+            
+            tokensOut[0] = IStableSwapPool(target).token();
+            return (true, tokensIn, tokensOut);
+        }
+
+        if (sig == EXCHANGE) {
+            // decode data
+            (uint256 i, uint256 j) = abi.decode(
+                data[4:],
+                (uint256, uint256)
             );
 
-        address[] memory tokensOut = new address[](1);
-        tokensOut[0] = IStableSwapPool(target).coins(i);
-        
-        return (
-            controllerFacade.isSwapAllowed(tokensIn[0]), 
-            tokensIn, 
-            tokensOut
-        );
+            address[] memory tokensIn = new address[](1);
+            tokensIn[0] = IStableSwapPool(target).coins(j);
+            
+            if (useEth) 
+                return (
+                    controllerFacade.isSwapAllowed(tokensIn[0]),
+                    tokensIn,
+                    new address[](0)
+                );
+
+            address[] memory tokensOut = new address[](1);
+            tokensOut[0] = IStableSwapPool(target).coins(i);
+            
+            return (
+                controllerFacade.isSwapAllowed(tokensIn[0]), 
+                tokensIn, 
+                tokensOut
+            );
+        }
+        return (false, new address[](0), new address[](0));
     }
 }
--- src/curve/IStableSwapPool.sol
+++ src/curve/IStableSwapPool.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.10;
 
 interface IStableSwapPool {
     function coins(uint256 i) external view returns (address);
+    function token() external view returns (address);
 }

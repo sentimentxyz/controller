45dbff057e8107bf50039a8061781a201ea2e355
r0ohafza <coldroohafza@gmail.com>
2022-06-28 10:47:29 -0700

    doc: compound controller

--- src/compound/CompoundController.sol
+++ src/compound/CompoundController.sol
@@ -1,18 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.15;
 
 import {ICToken} from "./ICToken.sol";
 import {IController} from "../core/IController.sol";
 
+/**
+    @title Compound Controller
+    @notice Controller for compound interaction
+*/
 contract CompoundController is IController {
+
+    /* -------------------------------------------------------------------------- */
+    /*                             CONSTANT VARIABLES                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice mint() function signature
     bytes4 constant MINT_ETH = 0x1249c58b;
+
+    /// @notice mint(uint256) function signature
     bytes4 constant MINT_ERC20 = 0xa0712d68;
+
+    /// @notice redeem(uint256) function signature
     bytes4 constant REDEEM = 0xdb006a75;
 
     // keccak256(abi.encodePacked("cETH"))
     bytes32 constant ethSymbol =
         0xb3c46c78043b5ff6963757142af6c297cddb5a0d3d823357472228eb35c8e890;
 
+    /* -------------------------------------------------------------------------- */
+    /*                              PUBLIC FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IController
     function canCall(address target, bool, bytes calldata data)
         external
         view

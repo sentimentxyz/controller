b7266c01b19dbc284aa7f6c90803bbe03c0b4889
r0ohafza <coldroohafza@gmail.com>
2022-08-11 17:35:24 -0700

    chore: cleanup

--- src/uniswap/UniV3LPController.sol
+++ src/uniswap/UniV3LPController.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import "forge-std/console.sol";
 import {Ownable} from "src/utils/Ownable.sol";
 import {IController} from "src/core/IController.sol";
 import {BytesLib} from "solidity-bytes-utils/BytesLib.sol";

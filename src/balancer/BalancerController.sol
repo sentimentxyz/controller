8b8593ae6f98c53331d7fd02ce6f758968e798d3
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-08-25 16:52:01 -0700

    SEN-216 | feat: add balancer controller (#34)
    * feat: add balancer controller
    
    * forge install: forge-std
    
    * feat: add balancer controller (exit and join)
    
    * fix: remove console.log
    
    * feat: add balancer controller (swap and batch swap)
    
    * fix: remove lib
    
    * feat: restrict batch swap to multihop
    
    * feat: add tests
    
    * forge install: forge-std
    
    * feat: add tests
    
    * doc: add natspec docs

---
+++ src/balancer/BalancerController.sol
@@ -0,0 +1,260 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "src/core/IController.sol";
+import "src/core/IControllerFacade.sol";
+import "./IVault.sol";
+
+/**
+    @title Balancer V2 Controller
+    @notice Balance v2 controller for join/exit/swap/batchSwap (multiHop)
+*/
+contract BalancerController is IController {
+
+    /* -------------------------------------------------------------------------- */
+    /*                             CONSTANT VARIABLES                             */
+    /* -------------------------------------------------------------------------- */
+
+    bytes4 constant JOIN = 0xb95cac28;
+    bytes4 constant EXIT = 0x8bdb3913;
+    bytes4 constant SWAP = 0x52bbbe29;
+    bytes4 constant BATCH_SWAP = 0x945bcec9;
+
+    /* -------------------------------------------------------------------------- */
+    /*                               STATE_VARIABLES                              */
+    /* -------------------------------------------------------------------------- */
+
+    IControllerFacade immutable controllerFacade;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+        @notice Contract constructor
+        @param _controller Controller Facade
+    */
+    constructor(IControllerFacade _controller) {
+        controllerFacade = _controller;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                              EXTERNAL FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IController
+    function canCall(address target, bool useEth, bytes calldata data)
+        external
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        bytes4 sig = bytes4(data);
+
+        if (sig == JOIN)
+            return canJoin(target, useEth, data[4:]);
+        if (sig == EXIT)
+            return canExit(target, useEth, data[4:]);
+        if (sig == SWAP)
+            return canSwap(target, useEth, data[4:]);
+        if (sig == BATCH_SWAP)
+            return canBatchSwap(target, useEth, data[4:]);
+        return (false, new address[](0), new address[](0));
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                             INTERNAL FUNCTIONS                             */
+    /* -------------------------------------------------------------------------- */
+
+    function canJoin(address target, bool, bytes calldata data)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        (
+            bytes32 poolId,
+            ,
+            ,
+            IVault.JoinPoolRequest memory request
+        ) = abi.decode(data, (
+                bytes32, address, address, IVault.JoinPoolRequest
+            )
+        );
+        address[] memory tokensIn = new address[](1);
+        address[] memory tokensOut = new address[](request.assets.length);
+
+        uint i; uint j;
+        while(i < request.assets.length) {
+            if (
+                request.maxAmountsIn[i] > 0 &&
+                address(request.assets[i]) != address(0)
+            )
+                tokensOut[j++] = address(request.assets[i]);
+            unchecked { ++i; }
+        }
+        assembly { mstore(tokensOut, j) }
+
+        (tokensIn[0],) = IVault(target).getPool(poolId);
+
+        return (
+            controllerFacade.isTokenAllowed(tokensIn[0]),
+            tokensIn,
+            tokensOut
+        );
+    }
+
+    function canExit(address target, bool, bytes calldata data)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        (
+            bytes32 poolId,
+            ,
+            ,
+            IVault.ExitPoolRequest memory request
+        ) = abi.decode(data, (
+                bytes32, address, address, IVault.ExitPoolRequest
+            )
+        );
+        address[] memory tokensOut = new address[](1);
+        address[] memory tokensIn = new address[](request.assets.length);
+
+        uint i; uint j;
+        while(i < request.assets.length) {
+            if (
+                request.minAmountsOut[i] > 0 &&
+                address(request.assets[i]) != address(0)
+            )
+                tokensIn[j++] = address(request.assets[i]);
+            unchecked { ++i; }
+        }
+        assembly { mstore(tokensIn, j) }
+
+        (tokensOut[0],) = IVault(target).getPool(poolId);
+
+        return (
+            true,
+            tokensIn,
+            tokensOut
+        );
+    }
+
+    function canSwap(address, bool, bytes calldata data)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        (
+            IVault.SingleSwap memory swap,
+            ,
+            ,
+        ) = abi.decode(data, (
+                IVault.SingleSwap, IVault.FundManagement, uint256, uint256
+            )
+        );
+
+        address[] memory tokensIn;
+        address[] memory tokensOut;
+
+        if (address(swap.assetIn) == address(0)) {
+            tokensIn = new address[](1);
+            tokensIn[0] = address(swap.assetOut);
+            return (
+                controllerFacade.isTokenAllowed(tokensIn[0]),
+                tokensIn,
+                new address[](0)
+            );
+        }
+
+        if (address(swap.assetOut) == address(0)) {
+            tokensOut = new address[](1);
+            tokensOut[0] = address(swap.assetIn);
+            return (
+                true,
+                new address[](0),
+                tokensOut
+            );
+        }
+
+        tokensIn = new address[](1);
+        tokensOut = new address[](1);
+        tokensOut[0] = address(swap.assetIn);
+        tokensIn[0] = address(swap.assetOut);
+
+        return (
+            controllerFacade.isTokenAllowed(tokensIn[0]),
+            tokensIn,
+            tokensOut
+        );
+    }
+
+    function canBatchSwap(address, bool, bytes calldata data)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        (
+            ,
+            IVault.BatchSwapStep[] memory swaps,
+            IAsset[] memory assets,
+            ,
+            ,
+        ) = abi.decode(data, (
+                uint8, IVault.BatchSwapStep[], IAsset[], IVault.FundManagement, uint256[], uint256
+            )
+        );
+
+        if (!isMultiHopSwap(swaps))
+            return (false, new address[](0), new address[](0));
+
+        uint tokenInIndex = swaps[swaps.length - 1].assetOutIndex;
+        uint tokenOutIndex = swaps[0].assetInIndex;
+
+        address[] memory tokensIn;
+        address[] memory tokensOut;
+
+        if (address(assets[tokenOutIndex]) == address(0)) {
+            tokensIn = new address[](1);
+            tokensIn[0] = address(assets[tokenInIndex]);
+            return (
+                controllerFacade.isTokenAllowed(tokensIn[0]),
+                tokensIn,
+                new address[](0)
+            );
+        }
+
+        if (address(assets[tokenInIndex]) == address(0)) {
+            tokensOut = new address[](1);
+            tokensOut[0] = address(assets[tokenOutIndex]);
+            return (
+                true,
+                new address[](0),
+                tokensOut
+            );
+        }
+
+        tokensIn = new address[](1);
+        tokensOut = new address[](1);
+        tokensOut[0] = address(assets[tokenOutIndex]);
+        tokensIn[0] = address(assets[tokenInIndex]);
+
+        return (
+            controllerFacade.isTokenAllowed(tokensIn[0]),
+            tokensIn,
+            tokensOut
+        );
+    }
+
+    function isMultiHopSwap(IVault.BatchSwapStep[] memory swaps)
+        internal
+        pure
+        returns (bool)
+    {
+        uint steps = swaps.length;
+        for (uint i; i < steps - 1; i++) {
+            if (swaps[i].assetOutIndex != swaps[i+1].assetInIndex)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ src/balancer/IVault.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+interface IAsset {}
+
+interface IVault {
+
+    function joinPool(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        JoinPoolRequest memory request
+    ) external payable;
+
+    struct JoinPoolRequest {
+        IAsset[] assets;
+        uint256[] maxAmountsIn;
+        bytes userData;
+        bool fromInternalBalance;
+    }
+
+    function exitPool(
+        bytes32 poolId,
+        address sender,
+        address payable recipient,
+        ExitPoolRequest memory request
+    ) external;
+
+    struct ExitPoolRequest {
+        IAsset[] assets;
+        uint256[] minAmountsOut;
+        bytes userData;
+        bool toInternalBalance;
+    }
+
+    function getPool(bytes32 poolId) external view returns (address, uint8);
+
+    function swap(
+        SingleSwap memory singleSwap,
+        FundManagement memory funds,
+        uint256 limit,
+        uint256 deadline
+    ) external payable returns (uint256);
+
+    struct SingleSwap {
+        bytes32 poolId;
+        uint8 kind;
+        IAsset assetIn;
+        IAsset assetOut;
+        uint256 amount;
+        bytes userData;
+    }
+
+    struct FundManagement {
+        address sender;
+        bool fromInternalBalance;
+        address payable recipient;
+        bool toInternalBalance;
+    }
+
+    function batchSwap(
+        uint8 kind,
+        BatchSwapStep[] memory swaps,
+        IAsset[] memory assets,
+        FundManagement memory funds,
+        int256[] memory limits,
+        uint256 deadline
+    ) external payable returns (int256[] memory);
+
+    struct BatchSwapStep {
+        bytes32 poolId;
+        uint256 assetInIndex;
+        uint256 assetOutIndex;
+        uint256 amount;
+        bytes userData;
+    }
+}

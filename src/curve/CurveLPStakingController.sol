edb22e26622670c08b68e669dbfbfdb86d21edf3
r0ohafza <coldroohafza@gmail.com>
2022-10-10 21:33:07 -0700

    feat: add functionality to claim on deposit and withdraw

--- src/curve/CurveLPStakingController.sol
+++ src/curve/CurveLPStakingController.sol
@@ -22,8 +22,14 @@ contract CurveLPStakingController is IController {
     /// @notice deposit(uint256)
     bytes4 constant DEPOSIT = 0xb6b55f25;
 
+    /// @notice deposit(uint256,address,bool)
+    bytes4 constant DEPOSITCLAIM = 0x83df6747;
+
     /// @notice withdraw(uint256)
     bytes4 constant WITHDRAW = 0x2e1a7d4d;
+
+    /// @notice withdraw(uint256,address,bool)
+    bytes4 constant WITHDRAWCLAIM = 0x00ebf5dd;
 
     /// @notice claim_rewards()
     bytes4 constant CLAIM = 0xe6f1daf2;
@@ -39,29 +45,87 @@ contract CurveLPStakingController is IController {
         returns (bool, address[] memory, address[] memory)
     {
         bytes4 sig = bytes4(data);
-        if (sig == DEPOSIT) {
-            address[] memory tokensIn = new address[](1);
-            address[] memory tokensOut = new address[](1);
-            tokensIn[0] = target;
-            tokensOut[0] = IChildGauge(target).lp_token();
-            return (true, tokensIn, tokensOut);
-        }
-        if (sig == WITHDRAW) {
-            address[] memory tokensIn = new address[](1);
-            address[] memory tokensOut = new address[](1);
-            tokensOut[0] = target;
-            tokensIn[0] = IChildGauge(target).lp_token();
-            return (true, tokensIn, tokensOut);
-        }
-        if (sig == CLAIM) {
-            uint count = IChildGauge(target).reward_count();
-            if (count == 0) return (false, new address[](0), new address[](0));
-            address[] memory tokensIn = new address[](count);
-            for (uint i; i<count; i++)
-                tokensIn[i] = IChildGauge(target).reward_tokens(i);
-            
-            return (true, tokensIn, new address[](0));
-        }
+
+        if (sig == DEPOSIT) canDeposit(target);
+        if (sig == DEPOSITCLAIM) canDepositAndClaim(target);
+        if (sig == WITHDRAW) canWithdraw(target);
+        if (sig == WITHDRAWCLAIM) canWithdrawAndClaim(target);
+        if (sig == CLAIM) canClaim(target);
+
         return (false, new address[](0), new address[](0));
+    }
+
+    function canDeposit(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](1);
+        address[] memory tokensOut = new address[](1);
+        tokensIn[0] = target;
+        tokensOut[0] = IChildGauge(target).lp_token();
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canDepositAndClaim(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        uint count = IChildGauge(target).reward_count();
+
+        address[] memory tokensIn = new address[](count + 1);
+
+        for (uint i; i<count; i++)
+            tokensIn[i] = IChildGauge(target).reward_tokens(i);
+        tokensIn[count] = target;
+
+        address[] memory tokensOut = new address[](1);
+        tokensOut[0] = IChildGauge(target).lp_token();
+
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canWithdraw(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](1);
+        address[] memory tokensOut = new address[](1);
+        tokensOut[0] = target;
+        tokensIn[0] = IChildGauge(target).lp_token();
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canWithdrawAndClaim(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        uint count = IChildGauge(target).reward_count();
+
+        address[] memory tokensIn = new address[](count + 1);
+        for (uint i; i<count; i++)
+            tokensIn[i] = IChildGauge(target).reward_tokens(i);
+        tokensIn[count] = IChildGauge(target).lp_token();
+
+        address[] memory tokensOut = new address[](1);
+        tokensOut[0] = target;
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canClaim(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        uint count = IChildGauge(target).reward_count();
+
+        address[] memory tokensIn = new address[](count);
+        for (uint i; i<count; i++)
+            tokensIn[i] = IChildGauge(target).reward_tokens(i);
+
+        return (true, tokensIn, new address[](0));
     }
 }

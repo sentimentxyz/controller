fd26c601b547bdc497f4135e135e10f5ee30b384
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-11-01 08:25:42 -0700

    feat: add balancer batch swap (#47)

--- src/balancer/BalancerController.sol
+++ src/balancer/BalancerController.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {IController} from "../core/IController.sol";
-import {IVault} from "./IVault.sol";
+import {IVault, IAsset} from "./IVault.sol";
 
 /**
     @title Balancer V2 Controller
@@ -22,9 +22,10 @@ contract BalancerController is IController {
 
     /// @notice swap((bytes32,uint8,address,address,uint256,bytes),(address,bool,address,bool),uint256,uint256)
     bytes4 constant SWAP = 0x52bbbe29;
+    bytes4 constant BATCH_SWAP = 0x945bcec9;
 
     /* -------------------------------------------------------------------------- */
-    /*                              EXTERNAL FUNCTIONS                              */
+    /*                              EXTERNAL FUNCTIONS                            */
     /* -------------------------------------------------------------------------- */
 
     /// @inheritdoc IController
@@ -41,6 +42,8 @@ contract BalancerController is IController {
             return canExit(target, useEth, data[4:]);
         if (sig == SWAP)
             return canSwap(target, useEth, data[4:]);
+        if (sig == BATCH_SWAP)
+            return canBatchSwap(target, useEth, data[4:]);
         return (false, new address[](0), new address[](0));
     }
 
@@ -166,5 +169,75 @@ contract BalancerController is IController {
             tokensIn,
             tokensOut
         );
+    }
+
+    function canBatchSwap(address, bool, bytes calldata data)
+        internal
+        pure
+        returns (bool, address[] memory, address[] memory)
+    {
+        (
+            ,
+            IVault.BatchSwapStep[] memory swaps,
+            IAsset[] memory assets,
+            ,
+            ,
+        ) = abi.decode(data, (
+                uint8, IVault.BatchSwapStep[], IAsset[], IVault.FundManagement, uint256[], uint256
+            )
+        );
+
+        if (!isMultiHopSwap(swaps))
+            return (false, new address[](0), new address[](0));
+
+        uint tokenInIndex = swaps[swaps.length - 1].assetOutIndex;
+        uint tokenOutIndex = swaps[0].assetInIndex;
+
+        address[] memory tokensIn;
+        address[] memory tokensOut;
+
+        if (address(assets[tokenOutIndex]) == address(0)) {
+            tokensIn = new address[](1);
+            tokensIn[0] = address(assets[tokenInIndex]);
+            return (
+                true,
+                tokensIn,
+                new address[](0)
+            );
+        }
+
+        if (address(assets[tokenInIndex]) == address(0)) {
+            tokensOut = new address[](1);
+            tokensOut[0] = address(assets[tokenOutIndex]);
+            return (
+                true,
+                new address[](0),
+                tokensOut
+            );
+        }
+
+        tokensIn = new address[](1);
+        tokensOut = new address[](1);
+        tokensOut[0] = address(assets[tokenOutIndex]);
+        tokensIn[0] = address(assets[tokenInIndex]);
+
+        return (
+            true,
+            tokensIn,
+            tokensOut
+        );
+    }
+
+    function isMultiHopSwap(IVault.BatchSwapStep[] memory swaps)
+        internal
+        pure
+        returns (bool)
+    {
+        uint steps = swaps.length;
+        for (uint i; i < steps - 1; i++) {
+            if (swaps[i].assetOutIndex != swaps[i+1].assetInIndex)
+                return false;
+        }
+        return true;
     }
 }

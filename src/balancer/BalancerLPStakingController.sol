1c7b171782fcbb5ff038d5f7b768c7e09b138487
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-11-01 08:25:28 -0700

    SEN-335 | Balancer lp staking controller (#46)
    * feat: add balancer lp staking
    
    * feat: add tests for balancer lp staking
    
    * fix: add arbi test workflow

---
+++ src/balancer/BalancerLPStakingController.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {IController} from "../core/IController.sol";
+
+interface IChildGauge {
+    function lp_token() external view returns (address);
+    function reward_tokens(uint256) external view returns (address);
+}
+
+/**
+    @title Curve LP staking controller
+    @notice Interaction controller for staking curve LP controllers
+*/
+contract BalancerLPStakingController is IController {
+
+    /* -------------------------------------------------------------------------- */
+    /*                               STATE VARIABLES                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice deposit(uint256)
+    bytes4 constant DEPOSIT = 0xb6b55f25;
+
+    /// @notice deposit(uint256,address,bool)
+    bytes4 constant DEPOSITCLAIM = 0x83df6747;
+
+    /// @notice withdraw(uint256)
+    bytes4 constant WITHDRAW = 0x2e1a7d4d;
+
+    /// @notice withdraw(uint256,address,bool)
+    bytes4 constant WITHDRAWCLAIM = 0x00ebf5dd;
+
+    /// @notice claim_rewards()
+    bytes4 constant CLAIM = 0xe6f1daf2;
+
+    /// @notice reward count
+    uint256 constant rewardsCount = 8;
+
+    /* -------------------------------------------------------------------------- */
+    /*                             EXTERNAL FUNCTIONS                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IController
+    function canCall(address target, bool, bytes calldata data)
+        external
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        bytes4 sig = bytes4(data);
+
+        if (sig == DEPOSIT) return canDeposit(target);
+        if (sig == DEPOSITCLAIM) return canDepositAndClaim(target);
+        if (sig == WITHDRAW) return canWithdraw(target);
+        if (sig == WITHDRAWCLAIM) return canWithdrawAndClaim(target);
+        if (sig == CLAIM) return canClaim(target);
+
+        return (false, new address[](0), new address[](0));
+    }
+
+    function canDeposit(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](1);
+        address[] memory tokensOut = new address[](1);
+        tokensIn[0] = target;
+        tokensOut[0] = IChildGauge(target).lp_token();
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canDepositAndClaim(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](rewardsCount + 1);
+        address reward; uint i;
+        for (; i<rewardsCount; i++) {
+            reward = IChildGauge(target).reward_tokens(i);
+            if (reward == address(0)) break;
+            tokensIn[i] = reward;
+        }
+        tokensIn[i] = target;
+        i++;
+        assembly { mstore(tokensIn, i) }
+
+        address[] memory tokensOut = new address[](1);
+        tokensOut[0] = IChildGauge(target).lp_token();
+
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canWithdraw(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](1);
+        address[] memory tokensOut = new address[](1);
+        tokensOut[0] = target;
+        tokensIn[0] = IChildGauge(target).lp_token();
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canWithdrawAndClaim(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](rewardsCount + 1);
+        address reward;
+        uint i;
+        for (; i<rewardsCount; i++) {
+            reward = IChildGauge(target).reward_tokens(i);
+            if (reward == address(0)) break;
+            tokensIn[i] = reward;
+        }
+        tokensIn[i] = IChildGauge(target).lp_token();
+        i++;
+        assembly { mstore(tokensIn, i) }
+
+        address[] memory tokensOut = new address[](1);
+        tokensOut[0] = target;
+        return (true, tokensIn, tokensOut);
+    }
+
+    function canClaim(address target)
+        internal
+        view
+        returns (bool, address[] memory, address[] memory)
+    {
+        address[] memory tokensIn = new address[](rewardsCount);
+        address reward;
+        uint i;
+        for (; i<rewardsCount; i++) {
+            reward = IChildGauge(target).reward_tokens(i);
+            if (reward == address(0)) break;
+            tokensIn[i] = reward;
+        }
+        assembly { mstore(tokensIn, i) }
+
+        return (true, tokensIn, new address[](0));
+    }
+}
---
+++ src/tests/BalancerLPStaking.t.sol
@@ -0,0 +1,204 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {TestBase} from "./utils/Base.t.sol";
+import {BalancerLPStakingController} from "../balancer/BalancerLPStakingController.sol";
+
+contract TestBalancerStakingArbi is TestBase {
+
+    BalancerLPStakingController balancerController;
+
+    address constant gauge = 0x5b6776cD9c51768Fc915caD7a7e8F5c4a6331131;
+    address constant lp = 0xFB5e6d0c1DfeD2BA000fBC040Ab8DF3615AC329c;
+    address constant r1 = 0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8;
+    address constant r2 = 0x13Ad51ed4F1B7e9Dc168d8a00cB3f4dDD85EfA60;
+
+    function setUp() public {
+        setupControllerFacade();
+        balancerController = new BalancerLPStakingController();
+        controllerFacade.updateController(gauge, balancerController);
+    }
+
+    function testCanDeposit() public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(gauge);
+
+        bytes memory data = abi.encodeWithSelector(0xb6b55f25,
+            0
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensIn[0], gauge);
+        assertEq(tokensOut[0], lp);
+    }
+
+    function testCannotDeposit() public {
+        bytes memory data = abi.encodeWithSelector(0xb6b55f25,
+            0
+        );
+
+        // Test
+        (bool canCall,,)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(!canCall);
+    }
+
+    function testCanDepositAndClaim() public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(gauge);
+        controllerFacade.toggleTokenAllowance(r1);
+        controllerFacade.toggleTokenAllowance(r2);
+
+        bytes memory data = abi.encodeWithSelector(0x83df6747,
+            0,
+            address(0),
+            true
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensIn[0], r1);
+        assertEq(tokensIn[1], r2);
+        assertEq(tokensIn[2], gauge);
+        assertEq(tokensIn.length, 3);
+        assertEq(tokensOut[0], lp);
+    }
+
+    function testCannotDepositAndClaim() public {
+        bytes memory data = abi.encodeWithSelector(0x83df6747,
+            0,
+            address(0),
+            true
+        );
+
+        // Test
+        (bool canCall,,)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(!canCall);
+    }
+
+    function testCanWithdrawAndClaim() public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(lp);
+        controllerFacade.toggleTokenAllowance(r1);
+        controllerFacade.toggleTokenAllowance(r2);
+
+        bytes memory data = abi.encodeWithSelector(0x00ebf5dd,
+            0,
+            address(0),
+            true
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensIn[0], r1);
+        assertEq(tokensIn[1], r2);
+        assertEq(tokensIn[2], lp);
+        assertEq(tokensIn.length, 3);
+        assertEq(tokensOut[0], gauge);
+    }
+
+    function testCannotWithdrawAndClaim() public {
+        bytes memory data = abi.encodeWithSelector(0x00ebf5dd,
+            0,
+            address(0),
+            true
+        );
+
+        // Test
+        (bool canCall,,)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(!canCall);
+    }
+
+    function testCanWithdraw() public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(lp);
+
+        bytes memory data = abi.encodeWithSelector(0x2e1a7d4d,
+            0
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensIn[0], lp);
+        assertEq(tokensOut[0], gauge);
+    }
+
+    function testCannotWithdraw() public {
+        bytes memory data = abi.encodeWithSelector(0x2e1a7d4d,
+            0
+        );
+
+        // Test
+        (bool canCall,,)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(!canCall);
+    }
+
+    function testCanClaim() public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(r1);
+        controllerFacade.toggleTokenAllowance(r2);
+
+        bytes memory data = abi.encodeWithSelector(0xe6f1daf2);
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensIn[0], r1);
+        assertEq(tokensIn[1], r2);
+        assertEq(tokensIn.length, 2);
+        assertEq(tokensOut.length, 0);
+    }
+
+    function testCannotClaim() public {
+        bytes memory data = abi.encodeWithSelector(0xe6f1daf2);
+
+        // Test
+        (bool canCall,,)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(!canCall);
+    }
+
+    function testCannotCall() public {
+        bytes memory data = abi.encodeWithSelector(0xe6f1aaf2);
+
+        // Test
+        (bool canCall,,)
+            = controllerFacade.canCall(gauge, true, data);
+
+        // Assert
+        assertTrue(!canCall);
+    }
+}

0c8f967619c9320478c223ed0162ea6e9a06e091
r0ohafza <coldroohafza@gmail.com>
2023-03-15 09:40:33 -0700

    feat: add 0x transform controller

---
+++ src/0x/TransformController.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {IController} from "../core/IController.sol";
+import {ITransformERC20Feature} from "./ITransform.sol";
+
+/**
+ * @title 0x V4 Controller
+ *     @notice 0x v4 controller for transformERC20
+ */
+contract TransformController is IController {
+    /* -------------------------------------------------------------------------- */
+    /*                             CONSTANT VARIABLES                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice transformERC20(address, address, uint256, uint256, (uint32,bytes)[])
+    bytes4 constant TRANSFORMERC20 = 0x415565b0;
+
+    /// @notice ETH address
+    address constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /* -------------------------------------------------------------------------- */
+    /*                              EXTERNAL FUNCTIONS                            */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IController
+    function canCall(address, bool, bytes calldata data)
+        external
+        pure
+        returns (bool, address[] memory tokensIn, address[] memory tokensOut)
+    {
+        bytes4 sig = bytes4(data);
+
+        if (sig != TRANSFORMERC20) {
+            return (false, new address[](0), new address[](0));
+        }
+
+        (address tokenOut, address tokenIn,,,) =
+            abi.decode(data[4:], (address, address, uint256, uint256, ITransformERC20Feature.Transformation[]));
+
+        if (tokenIn == ETH) {
+            tokensOut = new address[](1);
+            tokensOut[0] = tokenOut;
+            return (true, new address[](0), tokensOut);
+        }
+
+        if (tokenOut == ETH) {
+            tokensIn = new address[](1);
+            tokensIn[0] = tokenIn;
+            return (true, tokensIn, new address[](0));
+        }
+
+        tokensIn = new address[](1);
+        tokensOut = new address[](1);
+
+        tokensIn[0] = tokenIn;
+        tokensOut[0] = tokenOut;
+
+        return (true, tokensIn, tokensOut);
+    }
+}
---
+++ src/tests/0xTransform.t.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {TestBase} from "./utils/Base.t.sol";
+import {ITransformERC20Feature} from "../0x/ITransform.sol";
+import {TransformController} from "../0x/TransformController.sol";
+
+contract TestTransformController is TestBase {
+    TransformController transformController;
+
+    address target = makeAddr("target");
+
+    address constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    function setUp() public override {
+        super.setUp();
+        transformController = new TransformController();
+        controllerFacade.updateController(target, transformController);
+    }
+
+    function testCanTranformERC20(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut) public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(tokenIn);
+
+        ITransformERC20Feature.Transformation[] memory transformations = new ITransformERC20Feature.Transformation[](1);
+
+        bytes memory data = abi.encodeWithSelector(
+            ITransformERC20Feature.transformERC20.selector, tokenOut, tokenIn, amountIn, amountOut, transformations
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut) =
+            controllerFacade.canCall(target, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        if (tokenIn != ETH) {
+            assertEq(tokensIn[0], tokenIn);
+        } else {
+            assertEq(tokensIn.length, 0);
+        }
+        if (tokenOut != ETH) {
+            assertEq(tokensOut[0], tokenOut);
+        } else {
+            assertEq(tokensOut.length, 0);
+        }
+    }
+
+    function testCanTranformETHtoERC20(address tokenIn, uint256 amountIn, uint256 amountOut) public {
+        // Setup
+        controllerFacade.toggleTokenAllowance(tokenIn);
+
+        ITransformERC20Feature.Transformation[] memory transformations = new ITransformERC20Feature.Transformation[](1);
+
+        bytes memory data = abi.encodeWithSelector(
+            ITransformERC20Feature.transformERC20.selector, ETH, tokenIn, amountIn, amountOut, transformations
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut) =
+            controllerFacade.canCall(target, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensIn[0], tokenIn);
+        assertEq(tokensOut.length, 0);
+    }
+
+    function testCanTranformERC20ToETH(address tokenOut, uint256 amountIn, uint256 amountOut) public {
+        ITransformERC20Feature.Transformation[] memory transformations = new ITransformERC20Feature.Transformation[](1);
+
+        bytes memory data = abi.encodeWithSelector(
+            ITransformERC20Feature.transformERC20.selector, tokenOut, ETH, amountIn, amountOut, transformations
+        );
+
+        // Test
+        (bool canCall, address[] memory tokensIn, address[] memory tokensOut) =
+            controllerFacade.canCall(target, true, data);
+
+        // Assert
+        assertTrue(canCall);
+        assertEq(tokensOut[0], tokenOut);
+        assertEq(tokensIn.length, 0);
+    }
+}

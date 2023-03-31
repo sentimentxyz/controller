f1f7e5f3c9a8c8c242160860c277784903e6233f
r0ohafza <coldroohafza@gmail.com>
2023-03-31 15:17:06 +0400

    feat: delete controllers

--- src/core/ControllerFacade.sol
+++ src/core/ControllerFacade.sol
@@ -2,16 +2,13 @@
 pragma solidity ^0.8.17;
 
 import {Ownable} from "../utils/Ownable.sol";
-import {IController} from "./IController.sol";
-import {IControllerFacade} from "./IControllerFacade.sol";
 
 /**
-    @title Controller Facade
-    @notice This contract acts as a single interface for the client to determine
-    if a given interactions is acceptable
-*/
-contract ControllerFacade is Ownable, IControllerFacade {
-
+ * @title Controller Facade
+ *     @notice This contract acts as a single interface for the client to determine
+ *     if a given interactions is acceptable
+ */
+contract ControllerFacade is Ownable {
     /* -------------------------------------------------------------------------- */
     /*                               STATE VARIABLES                              */
     /* -------------------------------------------------------------------------- */
@@ -20,69 +17,56 @@ contract ControllerFacade is Ownable, IControllerFacade {
     mapping(address => bool) public isTokenAllowed;
 
     /// Mapping of external interaction with respective controller
-    mapping(address => IController) public controllerFor;
+    mapping(address => mapping(bytes4 => bool)) public isActionWhitelisted;
+
+    /// Mapping of external approve interaction with respective controller
+    mapping(address => bool) public isApproveAllowed;
 
     /* -------------------------------------------------------------------------- */
     /*                                   EVENTS                                   */
     /* -------------------------------------------------------------------------- */
 
-    event UpdateController(address indexed target, address indexed controller);
+    event UpdateWhitelist(address indexed target, bytes4 sig, bool isWhitelisted);
+    event UpdateApproval(address indexed target, bool isApproveAllowed);
+    event UpdateTokenAllowance(address indexed token, bool isTokenAllowed);
 
     /* -------------------------------------------------------------------------- */
     /*                                 CONSTRUCTOR                                */
     /* -------------------------------------------------------------------------- */
 
     /**
-        @notice Contract Constructor
-    */
+     * @notice Contract Constructor
+     */
     constructor() Ownable(msg.sender) {}
 
     /* -------------------------------------------------------------------------- */
     /*                              PUBLIC FUNCTIONS                              */
     /* -------------------------------------------------------------------------- */
 
-    /// @inheritdoc IControllerFacade
-    function canCall(
-        address target,
-        bool useEth,
-        bytes calldata data
-    )
-        external
-        view
-        returns (bool isValid, address[] memory tokensIn, address[] memory tokensOut)
-    {
-        (isValid, tokensIn, tokensOut) = controllerFor[target].canCall(target, useEth, data);
-        if (isValid) isValid = validateTokensIn(tokensIn);
+    function canCall(address target, bytes4 sig) external view returns (bool) {
+        return isActionWhitelisted[target][sig];
     }
 
-    /* -------------------------------------------------------------------------- */
-    /*                              INTERNAL FUNCTIONS                            */
-    /* -------------------------------------------------------------------------- */
-
-    function validateTokensIn(address[] memory tokensIn)
-        internal
-        view
-        returns (bool)
-    {
-        for (uint i; i < tokensIn.length; i++)
-            if (!isTokenAllowed[tokensIn[i]]) return false;
-        return true;
+    function canApprove(address target) external view returns (bool) {
+        return isApproveAllowed[target];
     }
-
 
     /* -------------------------------------------------------------------------- */
     /*                               ADMIN FUNCTIONS                              */
     /* -------------------------------------------------------------------------- */
 
-    function updateController(address target, IController controller)
-        external
-        adminOnly
-    {
-        controllerFor[target] = controller;
-        emit UpdateController(target, address(controller));
+    function updateWhitelist(address target, bytes4 sig, bool isWhitelisted) external adminOnly {
+        isActionWhitelisted[target][sig] = isWhitelisted;
+        emit UpdateWhitelist(target, sig, isWhitelisted);
     }
 
-    function toggleTokenAllowance(address token) external adminOnly {
-        isTokenAllowed[token] = !isTokenAllowed[token];
+    function updateApproval(address target, bool isAllowed) external adminOnly {
+        isApproveAllowed[target] = isAllowed;
+        emit UpdateApproval(target, isAllowed);
+    }
+
+    function toggleTokenAllowance(address token, bool isAllowed) external adminOnly {
+        isTokenAllowed[token] = isAllowed;
+        emit UpdateTokenAllowance(token, isAllowed);
     }
 }
--- src/core/IControllerFacade.sol
+++ src/core/IControllerFacade.sol
@@ -5,23 +5,6 @@ import {IController} from "./IController.sol";
 
 interface IControllerFacade {
     function isTokenAllowed(address token) external view returns (bool);
-    function controllerFor(address target) external view returns (IController);
-
-    /**
-        @notice General function that evaluates whether the target contract can
-        be interacted with using the specified calldata
-        @param target Address of external protocol/interaction
-        @param useEth Specifies if Eth is being sent to the target
-        @param data Calldata of the call made to target
-        @return canCall Specifies if the interaction is accepted
-        @return tokensIn List of tokens that the account will receive after the
-        interactions
-        @return tokensOut List of tokens that will be removed from the account
-        after the interaction
-    */
-    function canCall(
-        address target,
-        bool useEth,
-        bytes calldata data
-    ) external view returns (bool, address[] memory, address[] memory);
+    function canCall(address target, bytes4 sig) external view returns (bool);
+    function canApprove(address target) external view returns (bool);
 }
